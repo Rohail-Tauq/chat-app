6373205dbeb48c0346a3b98aef2cb2ef4923adb6
Rohail-Tauq <mrohail9932@gmail.com>
2025-07-12 02:20:28 -0700

    Fix Vite + Firebase env and config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -5,15 +6,13 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: "chat-app-dfe82",
-  storageBucket: "chat-app-dfe82.appspot.com",
-  messagingSenderId: "943551675512",
-  appId: "1:943551675512:web:cf89b77f95ef47cb25418d"
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
