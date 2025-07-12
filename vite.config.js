8c5ac8796af23c6c1492194f9b73e465d9488434
Rohail-Tauq <mrohail9932@gmail.com>
2025-07-12 02:42:17 -0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import nodePolyfills from 'vite-plugin-node-polyfills';
+import { NodeGlobalsPolyfillPlugin } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  optimizeDeps: {
-    include: ['firebase/auth', 'firebase/firestore', 'firebase/app']
-  }
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+      process: true,
+    }),
+  ],
 });
